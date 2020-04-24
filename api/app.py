3060f2b30f5c51e8c53fe38892d70490aa569a35
Zach <zlind@ucsc.edu>
2020-04-24 10:53:19 -0700

    added flask restful, serving sample json on aws

--- api/app.py
+++ api/app.py
@@ -1,10 +1,23 @@
-from flask import Flask
+from flask import Flask, make_response
+from flask_restful import Api, Resource
+import json
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route('/')
-def index():
-    return 'hello world', 200
+users = [
+    {'username': 'zlind', 'id': 0},
+    {'username': 'zberg', 'id': 1}
+]
+
+@api.resource('/')
+class Index(Resource):
+    def get(self):
+        return make_response(
+            json.dumps(users),
+            200,
+            {'content-type': 'application/json'}
+        )
 
 if __name__ == '__main__':
     app.run()
