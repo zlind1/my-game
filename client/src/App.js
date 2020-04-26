e30ed21844366f23d910fd5f974cd6a74a7661ae
Zach <zlind@ucsc.edu>
2020-04-25 22:59:48 -0700

    added react-icons, cleaned up login/signup screen

--- client/src/App.js
+++ client/src/App.js
@@ -1,64 +1,9 @@
 import React from 'react';
-import {Route, Link, Redirect} from 'react-router-dom';
-
-let IN_DEV = false;
-let awsApi = 'https://4enr01t30i.execute-api.us-west-1.amazonaws.com/dev';
-let localApi = 'http://localhost:5000';
-let apiUrl = (IN_DEV ? localApi : awsApi);
-const crypto = require('crypto');
+import {Route, Link} from 'react-router-dom';
+import Login from './Login.js';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.name = React.createRef();
-    this.pass = React.createRef();
-    this.state = {
-      toPlay: false
-    }
-  }
-  addUser = () => {
-    var username = this.name.current.value;
-    var password = crypto.createHmac('sha256', this.pass.current.value).digest('hex');
-    var user = {
-      username: username,
-      password: password
-    }
-    fetch(apiUrl+'/users', {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(user)
-    }).then(res => res.json()
-    .then(data => {
-      if (data.success) this.setState({toPlay: true});
-      else console.log('username taken');
-    }))
-  }
-  loginUser = () => {
-    var username = this.name.current.value;
-    var password = crypto.createHmac('sha256', this.pass.current.value).digest('hex');
-    var user = {
-      username: username,
-      password: password
-    }
-    fetch(apiUrl+'/users', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(user)
-    }).then(res => res.json()
-    .then(data => {
-      if (data.success) this.setState({toPlay: true});
-      else console.log('login failure');
-    }))
-  }
   render() {
-    if (this.state.toPlay) {
-      this.setState({toPlay: false});
-      return(<Redirect to='/play' />);
-    }
     return (
       <div className='App'>
         <Route exact path='/'>
@@ -70,14 +15,10 @@ class App extends React.Component {
           </Link>
         </Route>
         <Route exact path='/login'>
-          <input ref={this.name} placeholder='username'/>
-          <input ref={this.pass} type='password' placeholder='password'/>
-          <button onClick={this.loginUser}>Log in</button>
+          <Login login={true}/>
         </Route>
         <Route exact path='/signup'>
-          <input ref={this.name} placeholder='username'/>
-          <input ref={this.pass} type='password' placeholder='password'/>
-          <button onClick={this.addUser}>Sign up</button>
+          <Login login={false}/>
         </Route>
         <Route exact path='/play'>
           Play
---
+++ client/src/Login.js
@@ -0,0 +1,120 @@
+import React from 'react';
+import {Redirect, Link} from 'react-router-dom';
+import {Container, Form, InputGroup, Button} from 'react-bootstrap'
+import {FaUser, FaKey, FaEye} from 'react-icons/fa';
+
+let IN_DEV = false;
+let awsApi = 'https://4enr01t30i.execute-api.us-west-1.amazonaws.com/dev';
+let localApi = 'http://localhost:5000';
+let apiUrl = (IN_DEV ? localApi : awsApi);
+const crypto = require('crypto');
+
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.uref = React.createRef();
+    this.pref = React.createRef();
+    this.state = {
+      toPlay: false
+    }
+  }
+  UsernameInput = () => {
+    return (
+      <Form.Group>
+        <Form.Label>Username</Form.Label>
+        <InputGroup>
+          <InputGroup.Prepend>
+            <InputGroup.Text>
+              <FaUser />
+            </InputGroup.Text>
+          </InputGroup.Prepend>
+          <Form.Control ref={this.uref} placeholder='username'/>
+        </InputGroup>
+      </Form.Group>
+    );
+  }
+  PasswordInput = () => {
+    return (
+      <Form.Group>
+        <Form.Label>Password</Form.Label>
+        <InputGroup>
+          <InputGroup.Prepend>
+            <InputGroup.Text>
+              <FaKey />
+            </InputGroup.Text>
+          </InputGroup.Prepend>
+          <Form.Control ref={this.pref} type='password' placeholder='password'/>
+          <InputGroup.Append>
+            <InputGroup.Text>
+              <FaEye />
+            </InputGroup.Text>
+          </InputGroup.Append>
+        </InputGroup>
+      </Form.Group>
+    );
+  }
+  loginUser = () => {
+    var user = {
+      username: this.uref.current.value,
+      password: crypto.createHmac('sha256', this.pref.current.value).digest('hex')
+    }
+    fetch(apiUrl+'/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    }).then(res => res.json()
+    .then(data => {
+      if (data.success) this.setState({toPlay: true});
+      else console.log('login failure');
+    }))
+  }
+  addUser = () => {
+    var user = {
+      username: this.uref.current.value,
+      password: crypto.createHmac('sha256', this.pref.current.value).digest('hex')
+    }
+    fetch(apiUrl+'/users', {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    }).then(res => res.json()
+    .then(data => {
+      if (data.success) this.setState({toPlay: true});
+      else console.log('username taken');
+    }))
+  }
+  render() {
+    if (this.state.toPlay) {
+      return (<Redirect to='/play'/>);
+    }
+    return (
+      <Container className='d-flex flex-column justify-content-between py-3'>
+        <h1 className='align-self-center my-3'>
+          {this.props.login ? 'Log in to play' : 'Create an account'}
+        </h1>
+        <Form className='text-center align-self-center my-3'>
+          <this.UsernameInput />
+          <this.PasswordInput />
+          <Button onClick={this.props.login ? this.loginUser : this.addUser}
+          className='w-100 my-3'>
+            {this.props.login ? 'Log in' : 'Sign up'}
+          </Button>
+        </Form>
+        <p className='align-self-center my-3'>
+          {this.props.login ? 'No account yet? ' : 'Already have an account? '}
+          {this.props.login ? (
+            <Link to='/signup'>Sign up.</Link>
+          ) : (
+            <Link to='/login'>Log in.</Link>
+          )}
+        </p>
+      </Container>
+    );
+  }
+}
+
+export default Login;
